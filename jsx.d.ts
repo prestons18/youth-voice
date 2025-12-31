7a5331dc91e16fdba7f1e5a84ea2daaf28e11623
Preston Arnold <me@prestonarnold.uk>
2025-12-31 17:07:12 +0000

    Improve .d.ts and allow SVG

--- jsx.d.ts
+++ jsx.d.ts
@@ -2,27 +2,52 @@ export {};
 
 declare global {
   namespace JSX {
-    type HTMLTags = keyof HTMLElementTagNameMap;
-    
-    type EventHandlers<T extends HTMLElement> = {
-      [K in keyof HTMLElementEventMap as `on${Capitalize<string & K>}`]?: (
-        event: HTMLElementEventMap[K]
-      ) => void;
+    // FG reactivity primative
+    type MaybeFn<T> = T | (() => T);
+
+    // Base element model
+    type DOMElement = HTMLElement | SVGElement;
+
+    // HTML + SVG events
+    type EventHandlers<T extends Element> = {
+      [K in keyof GlobalEventHandlersEventMap as `on${Capitalize<
+        string & K
+      >}`]?: (event: GlobalEventHandlersEventMap[K]) => void;
     };
-    
-    type HTMLAttributes<T extends HTMLElement> = Partial<
-      Pick<T, 'style' | 'id' | 'title' | 'className'>
-    > & {
-      class?: string;
+
+    // Shared attributes
+
+    type BaseAttributes<T extends Element> = {
+      id?: MaybeFn<string>;
+      title?: MaybeFn<string>;
+
+      class?: MaybeFn<string>;
+      className?: MaybeFn<string>;
+
+      style?: MaybeFn<string | Partial<CSSStyleDeclaration>>;
+
+      ref?: (el: T | null) => void;
+
+      // Arbitary attributes (data-*, aria-*, etc.)
+      // Can also be reactive
       [attr: string]: any;
     };
-    
-    type Props<T extends HTMLElement> = HTMLAttributes<T> & EventHandlers<T>;
-    
-    type IntrinsicElements = {
+
+    type Props<T extends Element> = BaseAttributes<T> & EventHandlers<T>;
+
+    // Html Elements
+    type HTMLTags = keyof HTMLElementTagNameMap;
+    type HTMLElementProps = {
       [K in HTMLTags]: Props<HTMLElementTagNameMap[K]>;
     };
-    
-    interface IntrinsicElements extends IntrinsicElements {}
+
+    // SVG Elements
+    type SVGTags = keyof SVGElementTagNameMap;
+    type SVGElementProps = {
+      [K in SVGTags]: Props<SVGElementTagNameMap[K]>;
+    };
+
+    // JSX Instrict
+    interface IntrinsicElements extends HTMLElementProps, SVGElementProps {}
   }
 }
